08df56b08afcddb90826274293da0e22ed62cfdc
mohammed ali almaliky <3losh@localhost.localdomain>
2018-04-29 00:38:40 +0300

    add file emp

---
+++ emp.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE employees(emp_Id integer PRIMARY KEY , full_name text , b_date date, dept text , dept_n integer, user_name text, sal double, hire_date date);
+
+
+INSERT INTO employees VALUES(1,'mohammed ali','19-2-1998','markting',4,'emp',1000,'1-1-2017');
+
+
+
+
+SELECT * FROM employees;
