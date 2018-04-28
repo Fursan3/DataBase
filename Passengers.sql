11532875afe3e429c33dee7522b697af6df789e4
MustafaRaad <ofydesigner.2008@gmail.com>
2018-04-29 00:54:24 +0300

    edit passengers

--- Passengers.sql
+++ Passengers.sql
@@ -15,4 +15,29 @@ seat_no number(4),
 Arrive_time date,
     Cost number(4),
     CONSTRAINT pk_ticket primary key (ticket_id)
-)
+);
+drop sequence ticket_id;
+create sequence ticket_id
+start with 1 increment by 1;
+
+delete from PASSENGERS;
+
+insert into Passengers values(ticket_id.nextval,null,'Aqeel','Jabbar','m',to_date('23-02-1997','DD/MM/YYYY'),'770-567-5477',TO_DATE('30/04/2018 13:30', 'DD/MM/YYYY HH24:MI'),'Sulymania','one way',
+'C',7,55,TO_DATE('30/04/2018 14:30', 'DD/MM/YYYY HH24:MI'),null);
+
+insert into Passengers values(ticket_id.nextval,null,'Noor','ali','f',to_date('23-04-1997','DD/MM/YYYY'),'770-567-5477',TO_DATE('03/05/2018 15:30', 'DD/MM/YYYY HH24:MI'),'Najaf','two way',
+'A',5,7,TO_DATE('30/04/2018 17:30', 'DD/MM/YYYY HH24:MI'),null);
+
+insert into Passengers values(ticket_id.nextval,null,'Mustafa','Ahmed','m',to_date('23-04-1997','DD/MM/YYYY'),'770-567-5477',TO_DATE('30/04/2018 13:30', 'DD/MM/YYYY HH24:MI'),'Najaf','two way',
+'C',7,56,TO_DATE('30/04/2018 14:30', 'DD/MM/YYYY HH24:MI'),null);
+select * from passengers;
+
+
+
+
+
+
+
+
+
+
